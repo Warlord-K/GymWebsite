cfd2ab8844b3fe395d8f95d004bf261d6e2999e7
Warlord-K <95569637+Warlord-K@users.noreply.github.com>
2022-11-10 01:14:07 +0530

    Added Profile Page

--- profilePage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.decorators import login_required
-#now import the views.py file into this code
-from . import views
-urlpatterns=[
-  path('', login_required(views.profile), name='profile'),
-]
